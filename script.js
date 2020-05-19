3b4467c616468f049997fe36f03b2e1a55bb2fc2
Brian Buckley <25395464+bbuckley612@users.noreply.github.com>
2020-05-19 15:44:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function exportCanvas() {
   //https://stackoverflow.com/a/23684363
   var imgData = canvas.toDataURL("image/jpeg", 1.0);
   var pdf = new jsPDF();
-  pdf.addImage(imgData, 'JPEG', 0, 0);
+  //https://stackoverflow.com/a/36509632
+  var width = pdf.internal.pageSize.getWidth();
+  var height = pdf.internal.pageSize.getHeight();
+  //https://stackoverflow.com/a/23684363
+  pdf.addImage(imgData, 'JPEG', 0, 0, width, height);
   pdf.save("download.pdf");
 }
