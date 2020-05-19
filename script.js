2c5d55a0a7412390988ba31b5abf964c2b974fa1
Brian Buckley <25395464+bbuckley612@users.noreply.github.com>
2020-05-19 15:29:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var context = canvas.getContext('2d');
 function exportCanvas() {
   //https://stackoverflow.com/a/23684363
   var imgData = canvas.toDataURL("image/jpeg", 1.0);
+  var w = window.open("");
+  w.document.write(imgData);
   var pdf = new jsPDF();
   pdf.addImage(imgData, 'JPEG', 0, 0);
   pdf.save("download.pdf");
