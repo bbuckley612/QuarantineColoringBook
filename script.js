8df912ab5e29cdb36c4660915c363c004c23cef5
Brian Buckley <25395464+bbuckley612@users.noreply.github.com>
2020-05-19 16:14:26 -0400

    Update script.js
    Added CreateJS

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+const primary = 'white';
+const secondary = 'black';
+
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
+var stage = new createjs.Stage("canvas");
 
 function exportCanvas() {
   //https://stackoverflow.com/a/23684363
@@ -12,5 +16,15 @@ function exportCanvas() {
 }
 
 //https://stackoverflow.com/a/49745434
-context.fillStyle = 'black';
+context.fillStyle = secondary;
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+//https://www.createjs.com/easeljs
+function init() {
+  var circle = new createjs.Shape();
+  circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+  circle.x = 100;
+  circle.y = 100;
+  stage.addChild(circle);
+  stage.update();
+}
