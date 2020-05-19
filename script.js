8bad5622fc5a0eedc39791f067f07370a3f0ce74
Brian Buckley <25395464+bbuckley612@users.noreply.github.com>
2020-05-19 15:54:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var context = canvas.getContext('2d');
 function exportCanvas() {
   //https://stackoverflow.com/a/23684363
   var imgData = canvas.toDataURL("image/jpeg", 1.0);
-  var pdf = new jsPDF();
+  var pdf = new jsPDF('p', 'px', 'letter');
   //https://stackoverflow.com/a/36509632
   var width = pdf.internal.pageSize.getWidth();
   console.log(width);
